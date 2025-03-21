853e189c33f5bda9f54489082a98f5f3f4fa3ee8
Navedkhan06 <nkhan25790@gmail.com>
2025-03-21 10:31:27 +0530

    update

--- script.js
+++ script.js
@@ -44,4 +44,14 @@ window.onscroll = () => {
 
 }
 
+let slideIndex = 0;
+
+function moveSlide(n) {
+    let slides = document.querySelectorAll(".slide");
+    slideIndex += n;
+    if (slideIndex >= slides.length) slideIndex = 0;
+    if (slideIndex < 0) slideIndex = slides.length - 1;
+    document.querySelector(".slider").style.transform = `translateX(-${slideIndex * 100}%)`;
+}
+
 
